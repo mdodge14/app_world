fe1779f98053ecc96272a09076d854b57e8b10fb
Michael Dodge <mjdodge@dodgeco.com>
2021-09-03 11:07:21 -0400

    questions sequence

--- world_of_questions/models/question.py
+++ world_of_questions/models/question.py
@@ -9,7 +9,7 @@ _logger = logging.getLogger(__name__)
 class Question(models.Model):
     _name = "question"
     _description = "Question"
-    # _order = "name"
+    _order = "sequence, name"
 
     name = fields.Char(index=True, string='Question')
     sequence = fields.Integer('Sequence', index=True)
