d66d4fc4b9a9581ddb9585c10a9420e3737dcbc0
Michael Dodge <mjdodge@dodgeco.com>
2021-09-04 21:56:07 -0400

    challenge form cleanup

--- world_of_questions/models/challenge.py
+++ world_of_questions/models/challenge.py
@@ -13,26 +13,29 @@ class Challenge(models.Model):
 
     name = fields.Char(index=True)
     state = fields.Char(default="new")
-    message = fields.Char(default="Hi there, would you like to play 20 questions?", readonly=True)
+    message = fields.Char(default="Would you like to play 20 questions?", readonly=True)
     asked_question_ids = fields.Many2many('question', 'challenge_asked_questions', 'cid', 'qid')
 
     def yes_action(self):
         if self.state in ('new', 'not playing'):
-            self.name = "Great! I'll be the guesser. Do you know what you are?"
+            self.name = "Great! I'll be the guesser."
+            self.message = "Do you know what you are?"
             self.state = 'ready'
         elif self.state in ('ready', 'ask'):
             self.ask_question()
 
     def no_action(self):
         if self.state in ('new', 'ready'):
-            self.name = "OK, I'll be here when you're ready to play."
+            self.name = "OK, no problem."
+            self.message = "I'll be here when you're ready to play."
             self.state = 'not playing'
         elif self.state == 'ask':
             self.ask_question()
 
     def start_over(self):
         self.state = 'new'
-        self.name = "Hi there, would you like to play 20 questions?"
+        self.name = "Hi there!"
+        self.message = "Would you like to play 20 questions?"
         self.asked_question_ids = [(5, 0, 0)]
 
     def ask_question(self):
@@ -42,5 +45,6 @@ class Challenge(models.Model):
             question = self.env['question'].search([], order='sequence asc', limit=1)
         self.asked_question_ids = [(4, question.id)]
         number = len(self.asked_question_ids) if self.asked_question_ids else 1
-        self.name = "Got it. Here's question #{}: {}?".format(number, question.name)
+        self.name = "Question #{}".format(number)
+        self.message = "{}?".format(question.name)
         self.state = 'ask'
